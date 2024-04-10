d6fce2d07ac9d206d0b654a1cae186ca72036ea3
Monet <monetpaul23@gmail.com>
2024-04-10 19:50:05 -0400

    working on search

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export {default as  CocktailSkeleton } from "./CocktailSkeleton"
 export {default as   DetailsLoading } from "./DetailsLoading"
 export {default as   NotFound} from "./NotFound"
 export {default as CockktailThumbnails   } from "./CockktailThumbnails"
+export {default as   EmptySearch} from "./EmptySearch"
 
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import { Nav, NotFound } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Nav, NotFound, EmptySearch } from '../components';
+import axios from 'axios';
+
 
 
 const Search = ({searchbar, searchbarHandler}) => {
+const [list, setList] = useState([])
+  
+async function fetchCocktails(){
+    
+    const {data} = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchbar}`);
+    
+  console.log(data.drinks)
+  
+  }
+
+  useEffect(() => {
+    fetchCocktails();
+  }, [searchbar])
+  
+
+
+
   return (
     <div className='mx-auto  w-full max-w-7xl'>
     <Nav searchbar={searchbar} searchbarHandler={searchbarHandler} />
+   
 
     {/*  if  searchbar  is not  empty string */}
           {/* loading the search  */}
