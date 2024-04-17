4ba8d5a5c8f9ba231db0247eab7f667f53128c06
Monet <monetpaul23@gmail.com>
2024-04-17 14:32:00 -0400

    fix data for  foxtail  drink

--- src/assets/foxtailData.js
+++ src/assets/foxtailData.js
@@ -10,7 +10,7 @@ export default [
         strIngredient3: "Benedictine",
         strIngredient4: "Grapefriut Peel",
         strIngredient5: "small edible fox  and tree sculptures (optional)",
-        strInstructions:"Fill a shaker with ice cube. Add gin, grapefruit juice, and benedictine to the shaker and shake well. Strain the mixture into a cocktail glass. Twist a grapefruit peel over the drink and drop it in. If you have a fox and tree sculptures , place it on top of the drink carefully",
+        strInstructions:"Fill a shaker with ice cube. Add gin, grapefruit juice, and benedictine to the shaker and shake well. Strain the mixture into a cocktail glass. Twist a grapefruit peel over the drink and drop it in. If you have a fox and tree sculptures , place it on top of the drink carefully.",
         strMeasure1: "1 oz",
         strMeasure2: "1 oz",
         strMeasure3: "1 oz",
